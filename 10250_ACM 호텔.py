482b05a2b177275b3a6958ec68d68550318bd31f
bbookng <bbookng@gmail.com>
2022-07-23 18:06:50 +0900

    0723

---
+++ 10250_ACM 호텔.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(t):
+    h, w, n = map(int, input().split())
+    if n % h == 0:
+        floor = h * 100
+        distance = n // h
+    else:
+        floor = n % h * 100
+        distance = n // h + 1
+
+    print(floor + distance)
