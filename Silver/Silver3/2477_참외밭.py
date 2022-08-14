de4bdce7001e4b0fad8c40a11d5742ff8efd3790
bbookng <bbookng@gmail.com>
2022-08-14 23:47:05 +0900

    0814

--- Silver/Silver3/2477_참외밭.py
+++ Silver/Silver3/2477_참외밭.py
@@ -3,16 +3,17 @@ input = sys.stdin.readline
 
 K = int(input())
 arr = [list(map(int, input().split()))for _ in range(6)]
-max_C = max_R = 0
+max_C = max_R = c = r = 0
 
-for i in arr:
-    if i[0] < 3:
-        if i[1] > max_C:
-            max_C = i[1]
+for i in range(6):
+    if arr[i][0] < 3:
+        if arr[i][1] > max_C:
+            max_C = arr[i][1]
+            c = arr[i-3][1] if i > 2 else arr[i+3][1]
     else:
-        if i[1] > max_R:
-            max_R = i[1]
+        if arr[i][1] > max_R:
+            max_R = arr[i][1]
+            r = arr[i-3][1] if i > 2 else arr[i+3][1]
 
 
-
-print((max_C * max_R - minus) * K)
+print((max_C * max_R - c * r) * K)
