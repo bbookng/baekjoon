2264f08ddfd5ff7b5421f423fcecd58ac3775893
bbookng <bbookng@gmail.com>
2022-08-21 22:51:34 +0900

    0821

---
+++ Gold/Gold5/2293_동전1.py
@@ -0,0 +1,6 @@
+s = 'Reverse this strings'
+tmp = []
+for i in s:
+    tmp.insert(0, i)
+
+print(''.join(tmp))
---
+++ Silver/Silver5/14729_칠무해.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = [float(input()) for _ in range(N)]
+
+arr.sort()
+
+for i in range(7):
+    print('%.3f'%arr[i])
