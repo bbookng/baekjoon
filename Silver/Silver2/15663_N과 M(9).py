e9e637aaeccd1c94533446b82aef2a3da40f3a4d
bbookng <bbookng@gmail.com>
2022-09-02 01:29:24 +0900

    sslkj

--- Silver/Silver2/15663_N과 M(9).py
+++ Silver/Silver2/15663_N과 M(9).py
@@ -2,13 +2,25 @@ N, M = map(int,input().split())
 numbers = list(map(int, input().split()))
 numbers.sort()
 
-def solution(i, arr):
+check = set()
+chosen = [-1] * M
+visited = [0] * N
+
+def solution(i, M):
     if i == M:
-        print(" ".join(map(str, arr)))
+        print(" ".join(map(str, chosen)))
+        check.add(tuple(chosen))
         return
 
-    for j in numbers:
-        if not j in arr:
-            solution(i+1, arr + [j])
+    for j in range(len(numbers)):
+        for k in range(M):
+            if numbers[j] not in chosen:
+                chosen[k] = numbers[j]
+                visited[j] = 1
+                solution(i+1, M)
+                visited[j] = 0
 
-solution(0, [])
+
+
+
+solution(0, M)
--- permutation_sample_sky.py
+++ permutation_sample_sky.py
@@ -1,24 +1,28 @@
-# 한 행에서 column을 선택하고 다음 행으로 갈 때마다 이 함수가 불리운다.
-# chosen는 앞 선 행에서 선택된 column을 기록하고 있다.
+import requests
+from random import sample
 
-def perm(row, chosen) :
-    if row == N :                   # 마지막 열까지 왔으니
-        print('row == N(3)', chosen)
-        return
+URI = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1"
+lucky = requests.get(URI).json()
+answer = set()
+for i in range(1, 7):
+    answer.add(lucky[f'drwtNo{i}'])
+bonus = lucky['bnusNo']
 
-    for i in range(N) :
-        if i in chosen :
-            continue
-        chosen[row] = i
-        perm(row+1, chosen)
-        chosen[row] = -1
+result = [0] * 7
+for i in range(1000):
+    tmp = set(sample(list(range(1, 46)), 6))
+    n = answer & tmp
+    if len(n) == 6:
+        result[1] += 1
+    elif len(n) == 5 and bonus in tmp:
+        result[2] += 1
+    elif len(n) == 5:
+        result[3] += 1
+    elif len(n) == 4:
+        result[4] += 1
+    elif len(n) == 3:
+        result[5] += 1
+    else:
+        result[6] += 1
 
-
-
-N = 3
-data = [['A', 'B', 'C'],
-        ['D', 'E', 'F'],
-        ['G', 'H', 'I']]
-chosen = [-1 for _ in range(N)]     # 각 행에서 선택한 열을 기록하기 위함
-
-perm(0, chosen)
+print(result)
