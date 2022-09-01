e99dc9ad746a9868c25f3e646b3ef9656a66f150
bbookng <bbookng@gmail.com>
2022-09-01 17:57:12 +0900

    0901

--- Silver/Silver3/15654_N과 M(5).py
+++ Silver/Silver3/15654_N과 M(5).py
@@ -2,15 +2,17 @@ N, M = map(int,input().split())
 numbers = list(map(int, input().split()))
 numbers.sort()
 chosen = [-1] * M
+visited = [0] * N
+check = set()
 
 def solution(n, M):
     if n == M:
         print(" ".join(map(str, arr)))
+        check.add(tuple(chosen))
         return
 
     for j in numbers:
-        if not j in arr:
-            solution(i+1, arr + [j])
+
 
 
 solution(0, M)
