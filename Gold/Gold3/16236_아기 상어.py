30ceee7218389cc3a4b6522d86219acc6f00d919
bbookng <bbookng@gmail.com>
2022-09-23 17:57:39 +0900

    up

--- Gold/Gold3/16236_아기 상어.py
+++ Gold/Gold3/16236_아기 상어.py
@@ -12,13 +12,17 @@ for i in range(N):
         if board[i][j] == 9:
             board[i][j] = 0
             a, b = i, j
+            break
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+fish = []
+cnt = 0
+
 def bfs():
     visited = [[-1] * N for _ in range(N)]
-    q = deque([(a, b)])
+    q = deque([(a, b, 0)])
     visited[a][b] = 0
     while q:
         x, y = q.popleft()
@@ -26,47 +30,9 @@ def bfs():
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == -1 and board[nx][ny] <= size:
                 visited[nx][ny] = visited[x][y] + 1
-                q.append((nx, ny))
-    return visited
+                q.append((nx, ny, visited[nx][ny] + 1))
+                fish.append((nx, ny, visited[nx][ny] + 1))
 
-
-print(bfs())
-
-def find(visited):
-    x, y = 0, 0
-    tmp = float('inf')
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] != -1 and 1 <= board[i][j] < size:
-                if visited[i][j] < tmp:
-                    x, y = i, j
-                    tmp = visited[i][j]
-    if tmp == float('inf'):
-        return None
-    else:
-        return x, y, tmp
-
-result = 0
-ate = 0
-
-while True:
-    value = find(bfs())
-    print(value)
-    if value == None:
-        print(result)
-        break
-    else:
-        cx, cy = value[0], value[1]
-        result += value[2]
-        board[cx][cy] = 0
-        ate += 1
-        if ate >= size:
-            size += 1
-            ate = 0
-
-
-
-
-
+    if len(fish) > 0:
 
 
--- permutation_sample_sky.py
+++ permutation_sample_sky.py
@@ -1,43 +1,16 @@
-import sys
-import heapq
-
-input = lambda :sys.stdin.readline().strip()
-
-N, M = map(int, input().split())
+N, B = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+result = arr[:]
 
-def solution(i, j, di, dj):
-    def make_candidates(array, tmp):
-        for _ in range(2):
-            value, ci, cj = heapq.heappop(array)
-            tmp -= value
-            visited.append((ci, cj))
+cnt = 0
+while cnt != B:
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                result[i][j] += result[i][k] * result[k][i]
 
-            for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                ni, nj = ci + di, cj + dj
-                if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in visited and (-arr[ni][nj], ni, nj) not in q:
-                    heapq.heappush(candidates, (-arr[ni][nj], ni, nj))
-        return tmp
-
-    q = []
-    heapq.heappush(q, (-arr[i][j], i, j))
-    heapq.heappush(q, (-arr[i+di][j+dj], i+di, j+dj))
-
-    visited = []
-    candidates = []
-
-    res = make_candidates(q, 0)
-    res = make_candidates(candidates, res)
-
-    return res
-
-answer = 0
-for i in range(N-1):
-    for j in range(M):
-        answer = max(answer, solution(i, j, 1, 0))
-
+    cnt += 1
 for i in range(N):
-    for j in range(M-1):
-        answer = max(answer, solution(i, j, 0, 1))
-
-print(answer)
+    for j in range(N):
+        result[i][j] %= 1000
+print(result)
