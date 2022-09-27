04e4ea44ee437e7788bbaec4f227f17b5162b16f
bbookng <bbookng@gmail.com>
2022-09-27 19:13:28 +0900

    push

--- Silver/Silver1/11403_경로 찾기.py
+++ Silver/Silver1/11403_경로 찾기.py
@@ -1,20 +1,17 @@
 import sys
-
 input = sys.stdin.readline
-N = int(input())
-graph = []
 
-for _ in range(N):
-    graph.append(list(map(int,input().split())))
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
 
 for k in range(N):
     for i in range(N):
         for j in range(N):
             if graph[i][k] and graph[k][j]:
-                graph[i][j]=1
+                graph[i][j] = 1
 
-for r in graph:
-    print(*r)
+for i in graph:
+    print(*i)
 
 
 
--- Silver/Silver2/18870_좌표 압축.py
+++ Silver/Silver2/18870_좌표 압축.py
@@ -6,6 +6,7 @@ arr = list(map(int, input().split()))
 
 arr2 = sorted(list(set(arr)))
 dic = {arr2[i] : i for i in range(len(arr2))}
+print(dic)
 
 for i in arr:
     print(dic[i], end = ' ')
