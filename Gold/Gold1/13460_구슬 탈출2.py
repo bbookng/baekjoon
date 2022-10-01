0c796a1a4f7e5337bcec2260bb2425210808139d
bbookng <bbookng@gmail.com>
2022-10-01 10:51:06 +0900

    gold1 / 92mx

--- Gold/Gold1/13460_구슬 탈출2.py
+++ Gold/Gold1/13460_구슬 탈출2.py
@@ -3,28 +3,49 @@ input = lambda :sys.stdin.readline().strip()
 from collections import deque
 
 dx = [-1, 1, 0, 0]
-dy = [1, 1, 0, -1]
+dy = [0, 0, 1, -1]
+
+def move(x, y, i, j):
+    count = 0
+    while arr[x+i][y+j] != '#' and arr[x][y] != 'O':
+        x += i
+        y += j
+        count += 1
+    return x, y, count
 
 def bfs():
     q = deque()
-    visited = [[0] * M for _ in range(N)]
-    q.append(Red)
-    i = 0
+    visited = []
+    q.append((red_sx, red_sy, blue_sx, blue_sy, 1))
+    cnt = 0
     while q:
-        x, y = q.popleft()
-        print(x, y)
-        while True:
-            nx, ny = x + dx[i], y + dy[i]
-            # print(nx, ny)
-            if 0 <= nx < N and 0 <= ny < M:
-                if arr[nx][ny] == '#':
-                    i += 1
-                    i %= 4
-                elif arr[nx][ny] == 'O':
-                    return visited[nx][ny]
-                visited[nx][ny] = 1
-                q.append((nx, ny))
+        red_x, red_y, blue_x, blue_y, cnt = q.popleft()
 
+        if cnt > 10:
+            break
+
+        for i in range(4):
+            red_nx, red_ny, red_cnt = move(red_x, red_y, dx[i], dy[i])
+            blue_nx, blue_ny, blue_cnt = move(blue_x, blue_y, dx[i], dy[i])
+
+            if arr[blue_nx][blue_ny] == 'O':
+                continue
+
+            if arr[red_nx][red_ny] == 'O':
+                return cnt
+
+            if red_nx == blue_nx and red_ny == blue_ny:
+                if red_cnt > blue_cnt:
+                    red_nx -= dx[i]
+                    red_ny -= dy[i]
+                else:
+                    blue_nx -= dx[i]
+                    blue_ny -= dy[i]
+
+            if [red_nx, red_ny, blue_nx, blue_ny] not in visited:
+                visited.append([red_nx, red_ny, blue_nx, blue_ny])
+                q.append((red_nx, red_ny, blue_nx, blue_ny, cnt + 1))
+    return -1
 
 N, M = map(int, input().split())
 arr = [list(input()) for _ in range(N)]
@@ -32,10 +53,8 @@ arr = [list(input()) for _ in range(N)]
 for i in range(N):
     for j in range(M):
         if arr[i][j] == 'R':
-            Red = (i, j)
+            red_sx, red_sy = i, j
         elif arr[i][j] == 'B':
-            Blue = (i, j)
-        elif arr[i][j] == 'O':
-            hole = (i, j)
+            blue_sx, blue_sy = i, j
 
 print(bfs())
---
+++ Gold/Gold4/13913_숨바꼭질4.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(N):
+    q = deque([N])
+    visited[N][0] = 0
+    visited[N][1].append(1)
+
+    while q:
+        x = q.popleft()
+
+        for i in [x - 1, x + 1, x * 2]:
+            if 0 <= i <= 100000:
+                if visited[i][0] == -1:
+                    print(visited[i])
+                    visited[i][0] = visited[x][0] + 1
+                    visited[i][1] = visited[x][1] + [i]
+                    q.append(i)
+                # elif visited[i][0] == visited[x][0] + 1:
+                #     visited[i][1] += visited[x][1]
+
+N, K = map(int, input().split())
+visited = [[-1, []] for _ in range(100001)]
+
+bfs(N)
+print(visited[K][0])
+print(visited[K][1])
