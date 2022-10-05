3bfbd5e203d2ba600b5478d6e921e286ba072489
bbookng <bbookng@gmail.com>
2022-10-05 23:54:31 +0900

    1005

--- Gold/Gold5/1759_암호 만들기.py
+++ Gold/Gold5/1759_암호 만들기.py
@@ -1,1 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(cnt, idx):
+    if cnt == L:
+        mo, ja = 0, 0
+
+        for i in range(L):
+            if tmp[i] in ('a', 'e', 'i', 'o', 'u'):
+                mo += 1
+            else:
+                ja += 1
+
+        if mo >= 1 and ja >= 2:
+            print(''.join(tmp))
+        return
+
+    for i in range(idx, C):
+        tmp.append(alphabet[i])
+        dfs(cnt + 1, i + 1)
+        tmp.pop()
+
+
+L, C = map(int, input().split())
+alphabet = list(input().split())
+alphabet.sort()
+tmp = []
+dfs(0, 0)
 
