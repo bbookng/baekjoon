9d6b2985178a743bc6f3bcdf407a6ecdc2f8b405
bbookng <bbookng@gmail.com>
2022-10-13 13:32:17 +0900

    1013

---
+++ Silver/Silver3/14501_퇴사.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [0] * (N+1)
+
+for i in range(N-1, -1, -1):
+    if i + arr[i][0] > N:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], dp[i+arr[i][0]] + arr[i][1])
+
+print(dp[0])
+
