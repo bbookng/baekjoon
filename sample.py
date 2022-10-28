0ac6ce6d1fc41375e59e9088e3f080b91d8b40ec
bbookng <bbookng@gmail.com>
2022-10-28 23:55:43 +0900

    1028"

--- sample.py
+++ sample.py
@@ -1,54 +1,28 @@
-T = int(input())
+x = input()
 
-for tc in range(1, T+1):
-    N = int(input())
-    trees = list(map(int, input().split()))
-
-    trees.sort()
-    std = trees[-1]
-    flag = True
-    cnt = 0
-
-    while std in trees:
-        trees.pop()
-
-    while trees:
-        for i in range(len(trees)):
-            print(cnt, trees)
-            if flag:
-                if len(trees) == 1 and trees[0] == std-2:
-                    cnt += 1
-                    flag = False
-                    continue
-
-                if std-1 in trees:
-                    cnt += 1
-                    flag = False
-                    trees.remove(std-1)
-                    break
-                trees[i] += 1
-                flag = False
-                cnt += 1
-            else:
-                if std-2 in trees:
-                    flag = True
-                    trees.remove(std-2)
-                    cnt += 1
-                    break
-                trees[i] += 2
-                if trees[i] > std:
-                    trees[i] -= 2
-                    flag = True
-                    cnt += 1
-                    continue
-                flag = True
-                cnt += 1
-
-
-
-        while std in trees:
-            trees.pop()
-
-
-
-    print(cnt)
+if x == 'A+':
+    print(4.3)
+elif x == 'A0':
+    print(4.0)
+elif x == 'A-':
+    print(3.7)
+elif x == 'B+':
+    print(3.3)
+elif x == 'B0':
+    print(3.0)
+elif x == 'B-':
+    print(2.7)
+elif x == 'C+':
+    print(2.3)
+elif x == 'C0':
+    print(2.0)
+elif x == 'C-':
+    print(1.7)
+elif x == 'D+':
+    print(1.3)
+elif x == 'D0':
+    print(1.0)
+elif x == 'D-':
+    print(0.7)
+else:
+    print(0.0)
