4fed4cd8f4c217b784f4658fb5197b10b2e1ea8c
bbookng <bbookng@gmail.com>
2022-11-16 23:38:44 +0900

    update

--- sample.py
+++ sample.py
@@ -1,15 +1,19 @@
-N = int(input())
-arr = list(map(int, input().split()))
+import pandas as pd
+import requests
 
-cnt = 0
+URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=642cc263c19a3b2f7ab3f2c988a752ab&language=ko-KR&page='
 
-result = 0
+response = requests.get(URL+f"{1}")
+df = pd.DataFrame(response.json()["results"])
 
-for i in arr:
-    if i == 1:
-        cnt += 1
-        result += cnt
-    else:
-        cnt = 0
 
-print(result)
+for i in range(2, 3):
+    response = requests.get(URL+f"{i}")
+    tmp = pd.DataFrame(response.json()["results"])
+
+    df = pd.concat([df, tmp], ignore_index=True)
+    print(i)
+
+df = df[['adult', 'backdrop_path', 'genre_ids', 'id', 'original_language', 'overview', 'popularity', 'poster_path',
+       'release_date', 'title', 'video', 'vote_average', 'vote_count']]
+df.to_csv("data.csv", encoding='cp949')
