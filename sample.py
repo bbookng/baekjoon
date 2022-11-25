7244428c9d07504bfbcd887cfc3bbb27330ef9ce
bbookng <bbookng@gmail.com>
2022-11-25 16:35:51 +0900

    tmp

--- sample.py
+++ sample.py
@@ -17,3 +17,4 @@ for i in range(2, 3):
 df = df[['adult', 'backdrop_path', 'genre_ids', 'id', 'original_language', 'overview', 'popularity', 'poster_path',
        'release_date', 'title', 'video', 'vote_average', 'vote_count']]
 df.to_csv("data.csv", encoding='cp949')
+
