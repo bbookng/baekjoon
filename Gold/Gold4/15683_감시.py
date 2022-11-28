0c6da145ca8313301a31fd7ea5cc0b75216ca9ab
bbookng <bbookng@gmail.com>
2022-11-28 22:51:20 +0900

    tmp

--- Gold/Gold4/15683_감시.py
+++ Gold/Gold4/15683_감시.py
@@ -8,55 +8,31 @@ directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 methods = {1: ([0], [1], [2], [3]), 2: ([0, 3], [1, 2]), 3: ([2, 0], [1, 0], [1, 3], [3, 2] ), 4: ([0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]), 5: ([], [0, 1, 2, 3])}
 visited = [[0] * M for _ in range(N)]
 
-def solution(i, j):
-    global visited
-    max_ = 0
-    std = [i[:] for i in visited]
-    for case in methods[arr[i][j]]:
-        check = [i[:] for i in visited]
-        cnt = 0
-        for k in case:
-            dx, dy = directions[k]
-            x, y = i, j
-            while True:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] != 6:
-                    if arr[nx][ny] > arr[i][j]:
-                        cnt = 0
-                        break
-                    if not check[nx][ny]:
-                        check[nx][ny] = 1
-                        cnt += 1
-                        x, y = nx, ny
-                    else:
-                        x, y = nx, ny
-                        continue
-                else:
-                    break
-
-        if cnt > max_:
-            max_ = cnt
-            std = [i[:] for i in check]
-
-    visited = [i[:] for i in std]
-
-
-
+sensors = []
 for i in range(N):
     for j in range(M):
-        if arr[i][j] == 6:
-            visited[i][j] = 1
+        if 0 < arr[i][j] < 6:
+            sensors.append((i, j, arr[i][j]))
+
+K = len(sensors)
+
+def calculate(i, j, k):
+    ks = methods[arr[i][j]][k]
+    for k in ks:
+        try:
+            di, dj = i + directions[k], j + directions[k]
+
+        except:
             continue
-        elif arr[i][j] != 0:
-            visited[i][j] = 1
-            solution(i, j)
 
-result = 0
+_min = float('inf')
+def solution(i, total):
+    global _min
 
-for i in visited:
-    result += i.count(0)
-
-print(result)
+    if i == K:
+        _min = min(total, _min)
 
 
+    for j in methods[sensors[i][2]]:
 
+        solution(i+1, arr)
---
+++ Gold/Gold4/2448_별찍기 11.py
@@ -0,0 +1,22 @@
+n = int(input())
+graph = [[" "] * (2 * n) for _ in range(n)]
+
+
+def solution(x, y, z):
+    if z == 3:
+        graph[x][y] = "*"
+        graph[x + 1][y - 1] = graph[x + 1][y + 1] = "*"
+        for i in range(-2, 3):
+            graph[x + 2][y + i] = "*"
+        return
+
+    nz = z // 2
+    solution(x, y, nz)
+    solution(x + nz, y - nz, nz)
+    solution(x + nz, y + nz, nz)
+
+
+solution(0, n - 1, n)
+
+for row in graph:
+    print("".join(row))
