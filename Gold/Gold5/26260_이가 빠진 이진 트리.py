9523e7c7385897d09cdbbdc55354d55a8887146d
bbookng <bbookng@gmail.com>
2023-06-05 00:49:07 +0900

    [BOJ] 이가 빠진 이진 트리 / 골드5 / 316ms

--- Gold/Gold5/26260_이가 빠진 이진 트리.py
+++ Gold/Gold5/26260_이가 빠진 이진 트리.py
@@ -6,15 +6,31 @@ X = int(input())
 nodes[nodes.index(-1)] = X
 nodes.sort()
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-def solution(n, d):
-    if not d:
-        print(nodes[n], end=' ')
-        return
-    if nodes[n - d]:
-        solution(n - d, d//2)
-    if nodes[n + d]:
-        solution(n + d, d//2)
-    print(nodes[n], end=' ')
 
-solution(N // 2, (N+1)//4)
+def BST(nums):
+    if not nums:
+        return None
+
+    mid = len(nums) // 2
+    root = Node(nums[mid])
+
+    root.left = BST(nums[:mid])
+    root.right = BST(nums[mid + 1:])
+
+    return root
+
+tree = BST(nodes)
+
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end=' ')
+
+postorder(tree)
