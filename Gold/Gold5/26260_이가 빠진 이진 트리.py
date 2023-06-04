8ef0ce2c1f5a2b36ec1d90ab92ad7dbb7786df56
bbookng <bbookng@gmail.com>
2023-06-05 00:34:19 +0900

    [BOJ] 이가 빠진 이진 트리 / 골드5 / 푸는중

---
+++ Gold/Gold5/26260_이가 빠진 이진 트리.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+nodes = list(map(int, input().split()))
+X = int(input())
+
+nodes[nodes.index(-1)] = X
+nodes.sort()
+
+
+def solution(n, d):
+    if not d:
+        print(nodes[n], end=' ')
+        return
+    if nodes[n - d]:
+        solution(n - d, d//2)
+    if nodes[n + d]:
+        solution(n + d, d//2)
+    print(nodes[n], end=' ')
+
+solution(N // 2, (N+1)//4)
