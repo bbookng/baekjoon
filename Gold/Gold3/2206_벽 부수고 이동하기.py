4ebce914a335c4b9943eb526183bbdbbdb06f4c2
bbookng <bbookng@gmail.com>
2023-07-02 19:00:08 +0900

    [BOJ] 벽 부수고 이동하기 / Gold3 / 4312ms

--- Gold/Gold3/2206_벽 부수고 이동하기.py
+++ Gold/Gold3/2206_벽 부수고 이동하기.py
@@ -2,40 +2,29 @@ from collections import deque
 
 N, M = map(int, input().split())
 board = [list(map(int, input())) for _ in range(N)]
-
-answer = float('inf')
+visited = [[[0, 0] for _ in range(M)] for _ in range(N)]
 
 def bfs():
-    global answer
-    visited = [[0] * M for _ in range(N)]
-    visited[0][0] = 1
+    visited[0][0][0] = 1
 
-    q = deque([(0, 0, 1)])
+    q = deque()
+    q.append((0, 0, 0))
+
     while q:
-        x, y, d = q.popleft()
+        x, y, state = q.popleft()
+
         if x == N-1 and y == M-1:
-            answer = min(answer, d)
+            return visited[x][y][state]
 
         for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-                visited[nx][ny] = 1
-                if board[nx][ny]:
-                    continue
-                else:
-                    q.append((nx, ny, d+1))
+            if 0 <= nx < N and 0 <= ny < M:
+                if board[nx][ny] == 1 and state == 0:
+                    visited[nx][ny][1] = visited[x][y][0] + 1
+                    q.append((nx, ny, 1))
+                elif board[nx][ny] == 0 and visited[nx][ny][state] == 0:
+                    visited[nx][ny][state] = visited[x][y][state] + 1
+                    q.append((nx, ny, state))
+    return -1
 
-
-
-for i in range(N):
-    for j in range(M):
-        if board[i][j] == 1:
-            board[i][j] = 0
-            bfs()
-            board[i][j] = 1
-
-if answer == float('inf'):
-    print(-1)
-else:
-    print(answer)
-
+print(bfs())
