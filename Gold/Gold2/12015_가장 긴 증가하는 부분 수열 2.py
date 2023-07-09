f9ecba6e4dbfbb95a64ac4e653811663a269ece0
bbookng <bbookng@gmail.com>
2023-07-09 21:21:47 +0900

    [BOJ] 가장 긴 증가하는 부분 수열 2 / Gold2 / 푸는즁

--- Gold/Gold2/12015_가장 긴 증가하는 부분 수열 2.py
+++ Gold/Gold2/12015_가장 긴 증가하는 부분 수열 2.py
@@ -2,6 +2,15 @@ N = int(input())
 arr = list(map(int, input().split()))
 
 dp = [1] * N
+def solution(start, end, pivot):
+    if start > end:
+        return 0
+    if start == end:
+        return 1
+    solution(start, pivot)
+
+    return
+
 
 for i in range(1, N):
     for j in range(i):
