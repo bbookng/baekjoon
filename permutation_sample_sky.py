b008b3bbc6737ec5acaf98ca0edd7fe2046b7f46
bbookng <bbookng@gmail.com>
2024-02-14 23:56:13 +0900

    Revert "[BOJ] 01타일 / Silver3 / 340ms"
    This reverts commit f954b53cbc4259d186956413d237d5e1c0396677.

--- permutation_sample_sky.py
+++ permutation_sample_sky.py
@@ -1,3 +1,31 @@
-N = int(input())
-cars = [list(map(int, input().split())) for _ in range(3*N)]
+import sys
+sys.setrecursionlimit(10**6)
 
+result = ''
+
+def solution(n, m, x, y, r, c, k):
+    global result
+
+    if (k - abs(x-r) + abs(y-c)) % 2:
+        return 'impossible'
+
+    if abs(x-r) + abs(y-c) > k:
+        return 'impossible'
+
+    def dfs(xi, yi, route, cnt):
+        global result
+
+        if cnt == k and xi == r-1 and yi == c-1 and result > route:
+            result = route
+
+        for i in {'d': [1, 0], 'l': [0, -1], 'r': [0, 1], 'u': [-1, 0]}:
+            nx, ny = xi + i[0], yi + i[1]
+
+            if 0 <= nx < n and 0 <= ny < m and cnt < k:
+                dfs(nx, ny, route + i, cnt + 1)
+
+    dfs(x-1, y-1, '', 0)
+
+    return result
+
+print(solution())
