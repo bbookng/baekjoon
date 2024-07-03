04099998d478f2ef6a2df525457af8431555bd3e
bbookng <bbookng@gmail.com>
2024-07-03 23:48:04 +0900

    [BOJ] 퇴사 / Silver3 / 48ms

--- Silver/Silver3/14501_퇴사.py
+++ Silver/Silver3/14501_퇴사.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 dp = [0] * (N+1)
@@ -12,4 +9,5 @@ for i in range(N-1, -1, -1):
         dp[i] = max(dp[i+1], dp[i+arr[i][0]] + arr[i][1])
 
 print(dp[0])
+
 
